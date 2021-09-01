a4fefe84bb4fedc5c72347d4efb0c91a88a82dd9
Sidney Kohtaro Levine <73620652+koh415@users.noreply.github.com>
2021-09-01 03:08:27 +0000

    Add print functions to clarify python prefixes.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@
 number = int(input("Enter a decimal number: "))
 
 # Print functions with built in functions for computation
-print(number)
+print()
+print(number,'\n')
 # Prefix for binary is 0b
-print("Binary: ", bin(number))
+print("Prefix for binary is 0b")
+print("Binary: ", bin(number), '\n')
 # Prefix for Octal is 0o
-print("Octal: ", oct(number))
+print("Prefix for Octal is 0o")
+print("Octal: ", oct(number), "\n")
 # Prefix for Hexadecimal is 0x
+print("Prefix for Hexadecimal is 0x")
 print("Hexadecimal: ", hex(number))
